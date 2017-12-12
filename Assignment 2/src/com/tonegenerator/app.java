bbfb4ddf97f587a40e32ba5fcbcd73342eb14dc6
khyjb1995 <yong950423@stu.xawl.edu.cn>
2017-12-12 13:29:27 +0800

    Assignment2: btn msg learn

--- Assignment 2/src/com/tonegenerator/app.java
+++ Assignment 2/src/com/tonegenerator/app.java
@@ -1,4 +1,27 @@
 package com.tonegenerator;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class app {
+    private JPanel panel_Main;
+    private JButton button_Msg;
+
+    public app() {
+        button_Msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello World!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new app().panel_Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
