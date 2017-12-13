091ca1517e46f2272f6d8aa58fc42e83311067d2
khyjb1995 <yong950423@stu.xawl.edu.cn>
2017-12-13 12:57:31 +0800

    Assignment 2: PlayOrFile Button Function Construct

--- Assignment 2/src/com/tonegenerator/Main.java
+++ Assignment 2/src/com/tonegenerator/Main.java
@@ -1,8 +1,12 @@
 package com.tonegenerator;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
 
 public class Main {
     private JPanel panelMain;
@@ -23,10 +27,14 @@ public class Main {
     private JLabel amplitudeTxt;
     private JRadioButton readRadioButton;
     private JRadioButton readFileRadioButton;
-    private JTextField textField1;
-    private JButton button1;
+    private JTextField readTxt;
+    private JButton outputButton;
     private JPanel panelLeft;
     private JPanel panelRight;
+
+    AudioFormat audioFormat;
+    AudioInputStream audioInputStream;
+    SourceDataLine sourceDataLine;
 
     float sampleRate = 16000.0F;
     int sampleSizeInBits = 16;
@@ -41,7 +49,7 @@ public class Main {
             @Override
             public void actionPerformed(ActionEvent e) {
                 playorfileBtn.setEnabled(false);
-                new SynGen().getSyntheticData(audioData);
+                //new SynGen().getSyntheticData(audioData);
                 playorfileBtn.setEnabled(true);
             }
         });
@@ -53,11 +61,52 @@ public class Main {
         });
     }
 
+    private void playorfiledata() {
+        try{
+            InputStream byteArrayInputStream = new ByteArrayInputStream(audioData);
+
+            audioFormat = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+            audioInputStream = new AudioInputStream(byteArrayInputStream, audioFormat,
+                    audioData.length/audioFormat.getFrameSize());
+
+            DataLine.Info dataLineInfo =
+                    new DataLine.Info(
+                            SourceDataLine.class,
+                            audioFormat);
+
+            sourceDataLine = (SourceDataLine)
+                    AudioSystem.getLine(
+                            dataLineInfo);
+
+            if(readRadioButton.isSelected()){
+                new ListenThread().start();
+            }else{
+                generateBtn.setEnabled(false);
+                playorfileBtn.setEnabled(false);
+
+                try{
+                    AudioSystem.write(
+                            audioInputStream,
+                            AudioFileFormat.Type.WAVE,
+                            new File(readTxt.getText() + ".wav"));
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+                generateBtn.setEnabled(true);
+                playorfileBtn.setEnabled(true);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Tone Generator");
         frame.setContentPane(new Main().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        frame.setSize(500,500);
         frame.setVisible(true);
         new Main();
     }
